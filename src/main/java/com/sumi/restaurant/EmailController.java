70a94dd5a45ede137bea1a5b3034e67208dfaf90
ducduy <ducduy.chu.de@gmail.com>
2018-07-26 15:37:04 +0200

    change arrow

--- src/main/java/com/sumi/restaurant/EmailController.java
+++ src/main/java/com/sumi/restaurant/EmailController.java
@@ -6,16 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileInputStream;
 
 @RestController
 @RequestMapping("/send")
